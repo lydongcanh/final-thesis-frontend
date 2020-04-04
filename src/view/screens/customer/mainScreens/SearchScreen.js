5fd7c6211770ce764d65e0d6fc8f2b79d776d0c1
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-04-04 19:41:18 +0700

    Update customer search screen, load categories.

--- src/view/screens/customer/mainScreens/SearchScreen.js
+++ src/view/screens/customer/mainScreens/SearchScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { FlatList } from "react-native-gesture-handler";
 import { Layout, Text, Input, Icon, Button, Card } from "@ui-kitten/components";
-import { ActivityIndicator } from "react-native-paper";
+import { ActivityIndicator, Divider } from "react-native-paper";
 import { Image, Platform, StatusBar } from "react-native";
 import { CategoryService } from "../../../../core/services";
 import { Space, CustomerScreensHeader } from "../../../components/others";
@@ -9,6 +9,8 @@ import { Space, CustomerScreensHeader } from "../../../components/others";
 export default function SearchScreen({ navigation }) {
 
     const [categories, setCategories] = useState();
+    const [selectedMainCategory, setSelectedMainCategory] = useState();
+    const [selectedSubCategory, setSelectedSubCategory] = useState();
     const [isLoading, setIsLoading] = useState(true);
     const [isLoaded, setIsLoaded] = useState(false);
 
@@ -35,22 +37,91 @@ export default function SearchScreen({ navigation }) {
         } finally {
             setIsLoading(false);
         }
-    }
 
-    function setChildren(category) {
-        if (category && category.childrenCategories) {
-            category.children = category.childrenCategories;
-            for (const child of category.children) {
-                setChildren(child);
+        function setChildren(category) {
+            if (category && category.childrenCategories) {
+                category.children = category.childrenCategories;
+                for (const child of category.children) {
+                    setChildren(child);
+                }
             }
         }
     }
 
-    function handleCategoryClick(category) {
-        navigation.navigate("CustomerSubCategory", category);
+    function handleSubCategoryClick(category) {
+        setSelectedSubCategory(category);
     }
 
-    function getCategories() {
+    function handleMainCategoryClick(category) {
+        setSelectedMainCategory(category);
+        setSelectedSubCategory(null);
+    }
+
+    function getCategoriesUI() {
+        if (isLoading && !isLoaded)
+            return;
+
+        return (
+            <Layout>
+                {getMainCategoriesUI()}
+                {getSubCategoriesUI()}
+            </Layout>
+        );
+    }
+
+    function getMainCategoriesUI() {
+        return (
+            <Layout>
+                <Divider />
+                <FlatList
+                    horizontal
+                    data={categories}
+                    keyExtractor={(_, index) => index.toString()}
+                    renderItem={({ item }) => (
+                        <Button
+                            size="tiny"
+                            status={(selectedMainCategory && item.id === selectedMainCategory.id) ? "info" : "basic"}
+                            onPress={() => handleMainCategoryClick(item)}
+                            style={{ margin: 8, borderRadius: 24 }}
+                        >
+                            {item.name}
+                        </Button>
+                    )}
+                />
+                <Divider />
+            </Layout>
+        );
+    }
+
+    function getSubCategoriesUI() {
+        if (!selectedMainCategory || 
+            !selectedMainCategory.childrenCategories ||
+            selectedMainCategory.childrenCategories.length < 1)
+            return;
+
+        return (
+            <Layout>
+                <FlatList
+                    horizontal
+                    data={selectedMainCategory.childrenCategories.sort((a, b) => a.sortOrder > b.sortOrder)}
+                    keyExtractor={(_, index) => index.toString()}
+                    renderItem={({ item }) => (
+                        <Button
+                            size="tiny"
+                            status={(selectedSubCategory && item.id === selectedSubCategory.id) ? "info" : "basic"}
+                            onPress={() => handleSubCategoryClick(item)}
+                            style={{ margin: 8, borderRadius: 24 }}
+                        >
+                            {item.name}
+                        </Button>
+                    )}
+                />
+                <Divider />
+            </Layout>
+        );
+    }
+
+    function getContentUI() {
         if (isLoading) {
             return <ActivityIndicator style={{ margin: 8, flex: 1, alignContent: "center" }} />
         }
@@ -72,42 +143,24 @@ export default function SearchScreen({ navigation }) {
         }
 
         return (
-            <FlatList
-                data={categories}
-                numColumns={2}
-                keyExtractor={(_, index) => index.toString()}
-                renderItem={({ item }) => (
-                    <Card
-                        style={{ flex: 1, margin: 8 }}
-                        onPress={() => handleCategoryClick(item)}
-                    >
-                        <Image
-                            style={{ width: "100%", height: 150, justifyContent: "center", alignContent: "center" }}
-                            source={{ uri: item.imagePath }}
-                        />
-                        <Text
-                            category="h6"
-                            style={{ textAlign: "center", fontWeight: "bold" }}
-                        >
-                            {item.name}
-                        </Text>
-                    </Card>
-                )}
-            />
+            <Layout>
+                {/* <Input
+                    style={{ borderRadius: 24, marginLeft: 8, marginRight: 8 }}
+                    icon={(style) => <Icon {...style} name="search" />}
+                /> */}
+                {getCategoriesUI()}
+            </Layout>
         );
     }
 
     return (
-        <Layout style={{ 
-            flex: 1, 
-            justifyContent: "flex-start", 
-            marginTop: Platform.OS === "ios" ? 0 : StatusBar.currentHeight 
+        <Layout style={{
+            flex: 1,
+            justifyContent: "flex-start",
+            marginTop: Platform.OS === "ios" ? 0 : StatusBar.currentHeight
         }}>
-            {/* <Input
-                icon={(style) => <Icon {...style} name="search" />}
-            /> */}
             <CustomerScreensHeader navigation={navigation} />
-            {getCategories()}
+            {getContentUI()}
         </Layout>
     );
 }
